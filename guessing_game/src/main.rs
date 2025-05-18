153eb05bf8adaab2466a89c6c662849b408764d3
Robert Vitoriano <robertvitorianodev@gmail.com>
2025-05-18 10:35:28 -0300

    initial commit

---
+++ guessing_game/src/main.rs
@@ -0,0 +1,12 @@
+use std::io;
+
+fn main() {
+    println!("Gussing game started");
+    
+    let mut user_guess = String::new();
+    
+    io::stdin().read_line(&mut user_guess)
+        .expect("Failed to read line");
+
+    println!("{}", user_guess);
+    }
