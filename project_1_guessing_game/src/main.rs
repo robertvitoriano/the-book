af9c266b4aa8e4eb12afa1c0b893aa05df282978
Robert Vitoriano <robertvitorianodev@gmail.com>
2025-05-21 19:46:53 -0300

    finish game with pattern matching

--- project_1_guessing_game/src/main.rs
+++ project_1_guessing_game/src/main.rs
@@ -1,12 +1,30 @@
+use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 
 fn main() {
-    println!("Gussing game started");
-    
-    let mut user_guess = String::new();
-    
-    io::stdin().read_line(&mut user_guess)
-        .expect("Failed to read line");
+    println!("Guessing game started");
 
-    println!("{}", user_guess);
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+    
+    println!("Please enter a guess:");
+    
+    loop {
+        let mut user_guess = String::new();
+
+        io::stdin()
+            .read_line(&mut user_guess)
+            .expect("Failed to read line");
+
+        let user_guess: u32 = user_guess
+            .trim()
+            .parse()
+            .expect("Failed parsing user guess");
+
+        match user_guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => println!("You guessed it, the number is {}", secret_number),
+        }
     }
+}
